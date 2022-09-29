4b039f8c55414abe9f8fe806a1d55c7a7d5f5881
Assraf Nur <92791009+assraf-nur@users.noreply.github.com>
2022-09-29 14:54:16 +0600

    add a sub nav

--- src/Components/Navbar/SubNav.js
+++ src/Components/Navbar/SubNav.js
@@ -1,46 +1,95 @@
-import React from 'react';
-import { MdSpaceDashboard,MdCampaign } from 'react-icons/md';
-import { FaUserFriends,FaCalendarAlt } from 'react-icons/fa';
-import { RiTeamFill,RiUserSettingsFill } from 'react-icons/ri';
-import { TbReportAnalytics } from 'react-icons/tb';
-import { MdWifiCalling3,MdSettings } from 'react-icons/md';
-import '../CSS/Style.css'
+import React from "react";
+import { MdSpaceDashboard, MdCampaign } from "react-icons/md";
+import { FaUserFriends, FaCalendarAlt } from "react-icons/fa";
+import { RiTeamFill, RiUserSettingsFill } from "react-icons/ri";
+import { TbReportAnalytics } from "react-icons/tb";
+import { MdWifiCalling3, MdSettings } from "react-icons/md";
+import "../CSS/Style.css";
+import { Dropdown } from "react-bootstrap";
 
 const SubNav = () => {
-    return (
-        <div id='sub-nav' className='nav-container py-3'>
-            <div className='row sub-nav-menu'>
-                <div className='col-md-1'>
-                    <MdSpaceDashboard className='me-2 mb-1 sub-nav-icon'/> 
-                    Dashboard
-                </div>
-                <div className='col-md-1'>
-                    <FaUserFriends className='me-2 mb-1 sub-nav-icon'/>
-                    User</div>
-                <div className='col-md-1'>
-                    <RiTeamFill className='me-2 mb-1 sub-nav-icon'/>
-                    Team</div>
-                <div className='col-md-1'>
-                    <TbReportAnalytics className='me-2 mb-1 sub-nav-icon'/>
-                    Reports</div>
-                <div className='col-md-1'>
-                    <MdCampaign className='me-2 mb-1 sub-nav-icon'/>
-                    Campaign</div>
-                <div className='col-md-1'>
-                    <FaCalendarAlt className='me-2 mb-1 sub-nav-icon'/>
-                    Appointment</div>
-                <div className='col-md-1 ms-2'>
-                    <MdWifiCalling3 className='me-2 mb-1 sub-nav-icon'/>
-                    Contact</div>
-                <div className='col-md-1'>
-                    <MdSettings className='me-2 mb-1 sub-nav-icon'/>
-                    Settings</div>
-                <div className='col-md-2'>
-                    <RiUserSettingsFill className='me-2 mb-1 sub-nav-icon'/>
-                    Lender Settings</div>
-            </div>
+  return (
+    <div id="sub-nav" className="nav-container py-1">
+      <div className="row sub-nav-menu">
+        <div className="col-md-1">
+          <Dropdown className="">
+            <Dropdown.Toggle variant="normal" id="dropdown-basic">
+              <MdSpaceDashboard className="me-2 mb-1 sub-nav-icon" />
+              Dashboard
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
         </div>
-    );
+        <div className="col-md-1">
+          <Dropdown className="ms-3">
+            <Dropdown.Toggle variant="normal" id="dropdown-basic">
+              <FaUserFriends className="me-2 mb-1 sub-nav-icon" />
+              User
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
+        <div className="col-md-1 mt-2">
+          <RiTeamFill className="me-2 ms-2 mb-1 sub-nav-icon" />
+          Team
+        </div>
+        <div className="col-md-1">
+          <Dropdown className="report">
+            <Dropdown.Toggle variant="normal" id="dropdown-basic">
+              <TbReportAnalytics className="me-2 mb-1 sub-nav-icon" />
+              Reports
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
+        <div className="col-md-1">
+          <Dropdown className="campaign">
+            <Dropdown.Toggle variant="normal" id="dropdown-basic">
+              <MdCampaign className="me-2 mb-1 sub-nav-icon" />
+              Campaign
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
+        <div className="col-md-1 mt-2">
+          <FaCalendarAlt className="me-2 mb-1 sub-nav-icon" />
+          Appointment
+        </div>
+        <div className="col-md-1 mt-2 ms-2">
+          <MdWifiCalling3 className="me-2 mb-1 sub-nav-icon" />
+          Contact
+        </div>
+        <div className="col-md-1 mt-2">
+          <MdSettings className="me-2 mb-1 sub-nav-icon" />
+          Settings
+        </div>
+        <div className="col-md-2 mt-2">
+          <RiUserSettingsFill className="me-2 mb-1 sub-nav-icon" />
+          Lender Settings
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SubNav;
