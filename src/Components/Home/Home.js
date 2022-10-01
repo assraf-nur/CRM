3216a7fa2a750ece249e759f619b1f43bb6fb096
Assraf Nur <92791009+assraf-nur@users.noreply.github.com>
2022-10-01 15:38:01 +0600

    expo

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,6 +4,20 @@ const Home = () => {
     return (
         <div className='p-3'>
             <h1>Home sweet Home</h1>
+            <div className='row gap-4 px-3'>
+                <div className='col-xl border'>
+                    hello
+                </div>
+                <div className='col-xl border'>
+                    hello
+                </div>
+                <div className='col-xl border'>
+                    hello
+                </div>
+                <div className='col-xl border'>
+                    hello
+                </div>
+            </div>
         </div>
     );
 };
