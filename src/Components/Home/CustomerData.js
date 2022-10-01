e932b1c388e479439d396465d84dd41467ed537c
Assraf Nur <92791009+assraf-nur@users.noreply.github.com>
2022-10-01 18:38:45 +0600

    will set table

--- src/Components/Home/CustomerData.js
+++ src/Components/Home/CustomerData.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 const CustomerData = () => {
-  return ( b  
+  return ( 
     <div>
       <div className="row gap-4 px-3">
         <div className="col-xl border bg-light">
-
+          
         </div>
         <div className="col-xl border bg-light">hello</div>
       </div>
