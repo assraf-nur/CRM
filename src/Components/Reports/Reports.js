a62f5b85b51557db2e17c12be8a0641b8607719a
Assraf Nur <92791009+assraf-nur@users.noreply.github.com>
2022-10-03 10:44:48 +0600

    Revert "Revert "Merge branch 'main' of https://github.com/assraf-nur/CRM""
    This reverts commit e99a28eb277a6e6425dbbcf90522b3d8d1d49452.

---
+++ src/Components/Reports/Reports.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Reports = () => {
+    return (
+        <div className='p-3'>
+            <h1>Reports</h1>
+        </div>
+    );
+};
+
+export default Reports;
---
+++ src/Components/Users/Users.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Users = () => {
+    return (
+        <div className='p-3'>
+            <h1>Users</h1>
+        </div>
+    );
+};
+
+export default Users;
