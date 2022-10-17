b088ec20dbce0ba6e9c523679b50ecc49078c568
Assraf Nur <92791009+assraf-nur@users.noreply.github.com>
2022-10-17 18:02:08 +0600

    root

--- src/Components/Marketing/Marketing.js
+++ src/Components/Marketing/Marketing.js
@@ -1,17 +1,29 @@
 import React from "react";
+import { PieChart, Pie, Cell } from "recharts";
 import "../CSS/Style.css";
 import { HiPlus } from "react-icons/hi";
 import { RiDeleteBin6Line } from "react-icons/ri";
+import { GoPrimitiveDot } from "react-icons/go";
 import { BiEditAlt } from "react-icons/bi";
 
 const Marketing = () => {
+  const data = [
+    { name: "Total Message", value: 9000 },
+    { name: "Success Message", value: 3050 },
+    { name: "Failed Message", value: 6300 },
+  ];
+  const COLORS = ["#5156BE", "#00C49F", "#FFBB28"];
+
   return (
     <div className="p-3">
-      <div className="row gap-3 mx-1">
+      <div className="row gap-4 mx-1">
         <div className="col-xl-9 p-3 bg-white">
           <div className="box-head">
             <h5>Campaign List</h5>
-            <button style={{backgroundColor: "#5156BE"}} className="add-new-button py-1 px-2">
+            <button
+              style={{ backgroundColor: "#5156BE" }}
+              className="add-new-button py-1 px-2"
+            >
               <HiPlus className="me-1 mb-1" />
               Add New
             </button>
@@ -126,7 +138,7 @@ const Marketing = () => {
                     </a>
                   </td>
                 </tr>
-                <tr className="border-bottom">
+                <tr className="">
                   <td className="p-2 py-3">
                     <input
                       type="checkbox"
@@ -161,7 +173,56 @@ const Marketing = () => {
             </table>
           </div>
         </div>
-        <div className="col-xl p-3 bg-white"></div>
+        <div className="col-xl p-3 bg-white">
+          <div className="box-head">
+            <h5>SMS Summary</h5>
+          </div>
+          <div>
+            <PieChart width={290} height={200}>
+              <Pie
+                data={data}
+                cx={190}
+                cy={100}
+                innerText={"Nur"}
+                innerRadius={60}
+                outerRadius={80}
+                fill="#8884d8"
+                paddingAngle={0}
+                dataKey="value"
+              >
+                {data.map((entry, index) => (
+                  <Cell
+                    key={`cell-${index}`}
+                    fill={COLORS[index % COLORS.length]}
+                  />
+                ))}
+              </Pie>
+            </PieChart>
+          </div>
+          <div className="sms-pie-chart mx-1">
+            <div className="d-flex">
+              <GoPrimitiveDot style={{color: "#5156BE"}} className="fs-4 mt-3 me-1"/>
+              <div>
+                <p className="mb-0">9000</p>
+                <small className="mt-0">Total SMS</small>
+              </div>
+            </div>
+            <div className="d-flex">
+              <GoPrimitiveDot style={{color: "#00C49F"}} className="fs-4 mt-3 me-1"/>
+              <div>
+                <p className="mb-0">3050</p>
+                <small className="mt-0">Success SMS</small>
+              </div>
+            </div>
+            <div className="d-flex">
+              <GoPrimitiveDot style={{color: "#FFBB28"}} className="fs-4 mt-3 me-1"/>
+              <div>
+                <p className="mb-0">6300</p>
+                <small className="mt-0">Failed SMS</small>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
