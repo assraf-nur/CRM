424dab1cf6277aab21d792bf2b14fb07dff67fb6
Assraf Nur <92791009+assraf-nur@users.noreply.github.com>
2022-10-18 15:59:51 +0600

    er

--- src/Components/Setting/LenderList.js
+++ src/Components/Setting/LenderList.js
@@ -3,7 +3,8 @@ import React from 'react';
 const LenderList = () => {
     return (
         <div className='p-3'>
-            <h1>Lender List</h1>
+            <h4>Lender List</h4>
+            <iframe className='video-frame-commercial' width="720" height="420" src="https://www.youtube.com/embed/ED9A3NZY56Q" title="Top 7 Benefits of Semi-Truck Financing- Commercial Lending USA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
     );
 };
