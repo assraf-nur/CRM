0e28ab1c71dd3a83b8a7fc29660f49dbd26752cc
Assraf Nur <92791009+assraf-nur@users.noreply.github.com>
2022-10-22 11:48:23 +0600

    added todo list

--- src/Components/Contact/ContactDetails.js
+++ src/Components/Contact/ContactDetails.js
@@ -9,7 +9,7 @@ import { GoPrimitiveDot } from "react-icons/go";
 import { MdCancel, MdDelete } from "react-icons/md";
 import { FiCheckSquare, FiFilePlus } from "react-icons/fi";
 import { buildStyles, CircularProgressbar } from "react-circular-progressbar";
-import { Form, Offcanvas, ProgressBar } from "react-bootstrap";
+import { Card, Form, Offcanvas, ProgressBar } from "react-bootstrap";
 import profilePic from "../Images/Profile/profile-pic.png";
 import profilePic2 from "../Images/Profile/profile-pic2.png";
 import profilePic3 from "../Images/Profile/pro-icon.png";
@@ -423,8 +423,283 @@ const ContactDetails = () => {
                 </div>
               </form>
             </div>
-            <div className="toDo-list">
-              {/* will do saturday */}
+            <div className="toDo-list mt-3">
+              <div className="mt-4">
+                <Card style={{ width: "100%" }}>
+                  <Card.Body>
+                    <div className="note-upper-part">
+                      <div className="">
+                        <h6>
+                          {" "}
+                          <strong>Assigned To:</strong> Anne495{" "}
+                        </h6>
+                      </div>
+
+                      <div class="dropdown">
+                        <button
+                          class="note-option-button"
+                          type="button"
+                          data-bs-toggle="dropdown"
+                          aria-expanded="false"
+                        >
+                          <HiDotsVertical className="fs-5" />
+                        </button>
+                        <ul class="dropdown-menu">
+                          <li>
+                            <a class="dropdown-item" href="{#}">
+                              <FaEdit className="mb-1 me-1" /> Edit
+                            </a>
+                          </li>
+                          <li>
+                            <a class="dropdown-item" href="{#}">
+                              {" "}
+                              <MdDelete className="mb-1 me-1" /> Delete
+                            </a>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                    <h6>
+                      {" "}
+                      <strong>Priority:</strong> Low{" "}
+                    </h6>
+                    <h6>
+                      {" "}
+                      <strong>Due Date:</strong> 27-10-2022{" "}
+                    </h6>
+                    <h6>
+                      {" "}
+                      <strong>Status:</strong>{" "}
+                      <span className="status-active px-1 py-1 rounded">
+                        completed
+                      </span>{" "}
+                    </h6>
+                    <h6 className="mt-3">
+                      {" "}
+                      <strong>Summery:</strong>
+                    </h6>
+                    <Card.Text>
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </Card.Text>
+                    <h6 className="mt-3">
+                      {" "}
+                      <strong>Details:</strong>
+                    </h6>
+                    <Card.Text>
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </div>
+              <div className="mt-4">
+                <Card style={{ width: "100%" }}>
+                  <Card.Body>
+                    <div className="note-upper-part">
+                      <div className="">
+                        <h6>
+                          {" "}
+                          <strong>Assigned To:</strong> Anne495{" "}
+                        </h6>
+                      </div>
+
+                      <div class="dropdown">
+                        <button
+                          class="note-option-button"
+                          type="button"
+                          data-bs-toggle="dropdown"
+                          aria-expanded="false"
+                        >
+                          <HiDotsVertical className="fs-5" />
+                        </button>
+                        <ul class="dropdown-menu">
+                          <li>
+                            <a class="dropdown-item" href="{#}">
+                              <FaEdit className="mb-1 me-1" /> Edit
+                            </a>
+                          </li>
+                          <li>
+                            <a class="dropdown-item" href="{#}">
+                              {" "}
+                              <MdDelete className="mb-1 me-1" /> Delete
+                            </a>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                    <h6>
+                      {" "}
+                      <strong>Priority:</strong> Low{" "}
+                    </h6>
+                    <h6>
+                      {" "}
+                      <strong>Due Date:</strong> 27-10-2022{" "}
+                    </h6>
+                    <h6>
+                      {" "}
+                      <strong>Status:</strong>{" "}
+                      <span className="status-active px-1 py-1 rounded">
+                        completed
+                      </span>{" "}
+                    </h6>
+                    <h6 className="mt-3">
+                      {" "}
+                      <strong>Summery:</strong>
+                    </h6>
+                    <Card.Text>
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </Card.Text>
+                    <h6 className="mt-3">
+                      {" "}
+                      <strong>Details:</strong>
+                    </h6>
+                    <Card.Text>
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </div>
+              <div className="mt-4">
+                <Card style={{ width: "100%" }}>
+                  <Card.Body>
+                    <div className="note-upper-part">
+                      <div className="">
+                        <h6>
+                          {" "}
+                          <strong>Assigned To:</strong> Anne495{" "}
+                        </h6>
+                      </div>
+
+                      <div class="dropdown">
+                        <button
+                          class="note-option-button"
+                          type="button"
+                          data-bs-toggle="dropdown"
+                          aria-expanded="false"
+                        >
+                          <HiDotsVertical className="fs-5" />
+                        </button>
+                        <ul class="dropdown-menu">
+                          <li>
+                            <a class="dropdown-item" href="{#}">
+                              <FaEdit className="mb-1 me-1" /> Edit
+                            </a>
+                          </li>
+                          <li>
+                            <a class="dropdown-item" href="{#}">
+                              {" "}
+                              <MdDelete className="mb-1 me-1" /> Delete
+                            </a>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                    <h6>
+                      {" "}
+                      <strong>Priority:</strong> Low{" "}
+                    </h6>
+                    <h6>
+                      {" "}
+                      <strong>Due Date:</strong> 27-10-2022{" "}
+                    </h6>
+                    <h6>
+                      {" "}
+                      <strong>Status:</strong>{" "}
+                      <span className="status-active px-1 py-1 rounded">
+                        completed
+                      </span>{" "}
+                    </h6>
+                    <h6 className="mt-3">
+                      {" "}
+                      <strong>Summery:</strong>
+                    </h6>
+                    <Card.Text>
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </Card.Text>
+                    <h6 className="mt-3">
+                      {" "}
+                      <strong>Details:</strong>
+                    </h6>
+                    <Card.Text>
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </div>
+              <div className="mt-4">
+                <Card style={{ width: "100%" }}>
+                  <Card.Body>
+                    <div className="note-upper-part">
+                      <div className="">
+                        <h6>
+                          {" "}
+                          <strong>Assigned To:</strong> Anne495{" "}
+                        </h6>
+                      </div>
+
+                      <div class="dropdown">
+                        <button
+                          class="note-option-button"
+                          type="button"
+                          data-bs-toggle="dropdown"
+                          aria-expanded="false"
+                        >
+                          <HiDotsVertical className="fs-5" />
+                        </button>
+                        <ul class="dropdown-menu">
+                          <li>
+                            <a class="dropdown-item" href="{#}">
+                              <FaEdit className="mb-1 me-1" /> Edit
+                            </a>
+                          </li>
+                          <li>
+                            <a class="dropdown-item" href="{#}">
+                              {" "}
+                              <MdDelete className="mb-1 me-1" /> Delete
+                            </a>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                    <h6>
+                      {" "}
+                      <strong>Priority:</strong> Low{" "}
+                    </h6>
+                    <h6>
+                      {" "}
+                      <strong>Due Date:</strong> 27-10-2022{" "}
+                    </h6>
+                    <h6>
+                      {" "}
+                      <strong>Status:</strong>{" "}
+                      <span className="status-active px-1 py-1 rounded">
+                        completed
+                      </span>{" "}
+                    </h6>
+                    <h6 className="mt-3">
+                      {" "}
+                      <strong>Summery:</strong>
+                    </h6>
+                    <Card.Text>
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </Card.Text>
+                    <h6 className="mt-3">
+                      {" "}
+                      <strong>Details:</strong>
+                    </h6>
+                    <Card.Text>
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </div>
             </div>
           </div>
         </Offcanvas.Body>
